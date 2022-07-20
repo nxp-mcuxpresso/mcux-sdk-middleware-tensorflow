4ad910dea33f80034e243135843edffbe97792fb
Ladislav Vadkerti <ladislav.vadkerti@nxp.com>
2022-07-20 18:13:31 +0200

    Sync with MCUX SDK 2.12.0
    Signed-off-by: Ladislav Vadkerti <ladislav.vadkerti@nxp.com>

--- middleware_eiq_tensorflow_lite_micro_cmsis_nn.cmake
+++ middleware_eiq_tensorflow_lite_micro_cmsis_nn.cmake
@@ -13,8 +13,10 @@ target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
     ${CMAKE_CURRENT_LIST_DIR}/tensorflow/lite/micro/kernels/cmsis_nn/svdf.cpp
     ${CMAKE_CURRENT_LIST_DIR}/tensorflow/lite/micro/kernels/activations.cpp
     ${CMAKE_CURRENT_LIST_DIR}/tensorflow/lite/micro/kernels/floor.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/tensorflow/lite/micro/kernels/leaky_relu.cpp
     ${CMAKE_CURRENT_LIST_DIR}/tensorflow/lite/micro/kernels/logistic.cpp
     ${CMAKE_CURRENT_LIST_DIR}/tensorflow/lite/micro/kernels/quantize.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/tensorflow/lite/micro/kernels/transpose_conv.cpp
 )
 
 target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
